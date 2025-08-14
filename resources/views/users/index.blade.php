01f747e803010fdcfd117d5779076037256fc2da
Samuel Jr. <samuellima06091999@gmail.com>
2025-08-13 21:00:05 -0300

    Add default layout and refactor user index view to extend it

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,16 +1,21 @@
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
-        <title>User List</title>
-    </head>
-    <body>
-        <h1>Users</h1>
-        <ul>
-            @foreach ($users as $user)
-                <li>{{ $user->name }}</li>
-            @endforeach
-        </ul>
-    </body>
-</html>
+@extends('layouts.default')
+
+@section('content')
+<h1>Users</h1>
+<ul>
+    @foreach ($users as $user)
+        <li>{{ $user->name }}</li>
+    @endforeach
+
+    @php
+        $testing_char = 'Hello Worlds';
+    @endphp
+    @switch($testing_char)
+        @case('Hello World')
+            <p>{{ $testing_char }}</p>
+            @break
+        @default
+            <p>Unknown</p>
+    @endswitch
+</ul>
+
